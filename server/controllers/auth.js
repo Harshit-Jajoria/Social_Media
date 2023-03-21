c63c6f34a203335434905653eb0df2a4d8aa8c0e
Harshit-Jajoria <harshitjajoria66@gmail.com>
2023-03-21 19:14:07 +0530

    edits

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,7 +36,7 @@ export const register = async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({error});
   }
 };
 
